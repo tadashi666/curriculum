b9e3b04708972bc7d5c4b9af47b8864cdb0779d1
tadashi666 <66359887+tadashi666@users.noreply.github.com>
2020-09-14 14:22:05 +0900

    java study 1-4

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -25,7 +25,7 @@ public class Task1_4 {
     public static void main(String[] args) {
         // 以下の変数「name（名前）」「pass（パスワード）」を使用して、②〜⑤の条件を満たす処理を記述してください。
         // 名前とパスワードは画面からの入力値だと想定してみましょう。
-         String name = "alice";
+         String name = "alic";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
         if (name.equals("alice") && pass.equals("pass")){
@@ -33,12 +33,12 @@ public class Task1_4 {
         }
 
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        else if (name.equals("alice")){
+        else if (name.equals("alice") && (!( pass.equals("pass")))){
           System.out.println(CONST_MSG_ERROR_PASS);
         }
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        else if (pass.equals("pass")){
+        else if (pass.equals("pass") && (!( name.equals("alice")))){
           System.out.println (CONST_MSG_ERROR_NAME);
         }
 
